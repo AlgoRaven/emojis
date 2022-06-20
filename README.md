be1d7fbf04f9509542be5be7ff73abcdc85a584d
Anthony Krivonos <abkrivonos@gmail.com>
2022-06-20 01:28:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# emojis
+
+[<img src="https://badge.fury.io/js/%40algoraven%2Femojis.svg" />](https://www.npmjs.com/package/@algoraven/emojis)
+
+Dynamically served emojis borrowed from [iamcal/emojidata](https://github.com/iamcal/emoji-data).
+
+> ⚠️ Manual publishes are required to update the NPM library. No CI/CD is performed on merge.
+
+This is a public repository.
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import fetch from 'node-fetch'
+
+if (!globalThis.fetch) {
+	;(globalThis as any).fetch = fetch
+}
+
+export enum EmojiSet {
+	APPLE_HI_RES = 'img-apple-160',
+	APPLE_LO_RES = 'img-apple-64',
+	TWITTER_HI_RES = 'img-twitter-72',
+	TWITTER_LO_RES = 'img-twitter-64',
+}
+
+const CONFIG = {
+	OWNER: 'AlgoRaven',
+	REPO: 'emojis',
+	BRANCH: 'main',
+	USER_AGENT: 'algoraven.com',
+}
+
+const BASE_URL = `https://github.com/${CONFIG.OWNER}/${CONFIG.REPO}/blob/${CONFIG.BRANCH}/data`
+
+const cleanEmojiCode = (code: string) => {
+	code = code.replace('-fe0f', '')
+	return code
+}
+
+export const getEmojiUrl = (set: EmojiSet, code: string) => {
+	return `${BASE_URL}/${set}/${cleanEmojiCode(code)}.png?raw=true`
+}
+
+export class Emojis {
+	private set: EmojiSet
+
+	constructor(set: EmojiSet) {
+		this.set = set
+	}
+
+	public getEmojiUrl = (code: string) => getEmojiUrl(this.set, code)
+}
